654968601feffe00b10574bd871708df6286fe54
yeboaspral89 <yeboaspral19@gmail.com>
2021-01-11 16:00:20 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/githubproject/ItemDetailHostActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.githubproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.android.githubproject.databinding.ActivityItemDetailBinding;
+
+public class ItemDetailHostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityItemDetailBinding binding = ActivityItemDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_item_detail);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.
+                Builder(navController.getGraph())
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_item_detail);
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+}
